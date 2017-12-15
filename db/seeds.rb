c29e3fd79e22f0641f14a97d1f777f8f035603ff
Bernorts <a01209023@itesm.mx>
2017-12-15 12:50:50 -0600

    Roles agregados

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Role.find_or_create_by(name: 'Administrador')
+Role.find_or_create_by(name: 'Profesor')
+Role.find_or_create_by(name: 'Estudiante')
