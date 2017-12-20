86c126be7dce2072c886012672acbe2dfef0d516
MartinMolinero <a01204818@itesm.mx>
2017-12-20 13:09:13 -0600

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def editUser
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(password_params)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/usuario/:id', to: 'users#show', as: :show_user
   get '/usuarios/nuevo', to: 'users#new', as: :new_user
   get '/usuario/:id/editar', to: 'users#edit', as: :edit_user
-  patch '/usuario/:id/editUserData', to: 'users#editUserData', as: :edit_user_data
+  patch '/usuario/:id/editUserData', to: 'users#editUser', as: :edit_user_data
   post '/usuarios', to: 'users#create', as: :users
   patch '/usuario/:id', to: 'users#update', as: :update_user
   delete '/usuario/:id', to: 'users#delete', as: :delete_user
