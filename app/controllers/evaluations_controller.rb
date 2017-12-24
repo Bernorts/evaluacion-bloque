e4be001467da5cf3a8dc74a6d087b1f2cee748ac
Bernorts <a01209023@itesm.mx>
2017-12-23 21:47:16 -0600

    Editar evaluaciones casi listo

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -7,12 +7,11 @@ class EvaluationsController < ApplicationController
 	end
 
 	def create
+		@error = false
 		@interview = Interview.new(evaluated: false)
 		if !(@interview.save)
 			@error = true
 		end
-		@interview.save
-		@error = false
 		@reqDate = params[:evaluation][:reqDate]
 		@competences = params[:evaluation][:competences]
 
@@ -63,7 +62,51 @@ class EvaluationsController < ApplicationController
   	end
 
 	def update
-		@evaluation = evaluation.find(params[:id])
+		@error = false
+		@interview = interview.find(params[:id])
+		@reqDate = params[:evaluation][:reqDate]
+		@competences = params[:evaluation][:competences]
+
+		@competences.each do |key, value|
+			@competence_id = key.to_i
+			@desLevel_id = value[:level].to_i
+			@evidences = value[:evidences]
+			@evaluation_id = value[:evaluation_id]
+
+			@evaluation =  Evaluation.find(@evaluation_id)
+
+			if @desLevel_id === 0
+				@level_id = Evaluation.where(competence_id: @competence_id).last.achLevel
+				@evaluation.update_attributes(achLevel: @level_id, desLevel: 0, reqDate: @reqDate)
+			else
+				@level_id = @desLevel
+				@evaluation.update_attributes(achLevel: nil, desLevel: 0, reqDate: @reqDate)
+			end
+
+			
+			if !(@evaluation.save)
+				@error = true
+				break
+			elsif @evidences && @desLevel_id != 0
+				@evidences.each do |e|
+					@evaluation_evidence = EvaluationEvidence.new(evaluation_id: @evaluation.id, evidence_id: e.to_i)
+					if !@evaluation_evidence.save
+						@error = true
+						break
+					end
+				end
+			end
+		end
+			
+		if !@error
+			flash[:success] = 'Solicitud de evaluación registrada exitosamente'
+			redirect_to '/malla'
+		else
+			flash[:danger] = 'Ocurrió un error al guardar tu solicitud de evaluación. Inténtalo de nuevo'
+			render :action => 'new'
+		end
+
+
 		if @evaluation.update_attributes(evaluation_params)
 		    flash[:success] = 'Solicitud de revisión modificada exitosamente'
 		    redirect_to '/malla'
