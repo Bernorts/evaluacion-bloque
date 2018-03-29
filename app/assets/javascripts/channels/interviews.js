7473aea4dcd8fbaa0b579474662516caa0165990
MartinMolinero <a01204818@itesm.mx>
2018-03-29 15:33:02 -0600

    interview

--- app/assets/javascripts/channels/interviews.js
+++ app/assets/javascripts/channels/interviews.js
@@ -1,5 +1,5 @@
+console.log("DICKS")
 App.interviews = App.cable.subscriptions.create('InterviewsChannel', {
-  console.log("YES")
   received: function(data) {
     $("#interviews").removeClass('hidden')
     return $('#interviews').append(this.renderMessage(data));
