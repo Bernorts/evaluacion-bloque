8a38e2f13836ebd10de4691a474ddecb0f1324f3
Bernorts <a01209023@itesm.mx>
2018-04-12 22:39:11 -0500

    Data almost ready for ajax in interviews

--- app/assets/javascripts/channels/interviews.js
+++ app/assets/javascripts/channels/interviews.js
@@ -27,3 +27,133 @@ App.interviews = App.cable.subscriptions.create('InterviewsChannel', {
     return row;
   }
 });
+
+/*
+
+$.ajax({
+    url: $(this).attr("href"),
+    success: function(data) {
+      console.log("DATA", data);
+        $(data).appendTo(".modal-lg");
+        changeSubmitToAjax('#editEvidence', '.edit_evidence', "Edición exitosa!");
+        $('#editEvidence').modal('show');
+    }
+});
+
+*/
+
+
+$( document ).ready(function() {
+	var competence_id;
+	var professor_id;
+	var role;
+	var level_id;
+	var retro;
+	var final_level;
+	var responsible_id;
+
+	$('.inter-role-select').on('change', function() {
+   		role = this.value;
+   		competence_id = $(this).data("comp");
+   		professor_id = $(this).data("evaluator");
+
+   		if(role == 'true'){
+   			$(this).attr('data-responsible', 'true');
+   		} else{
+   			$(this).removeAttr('data-responsible');
+   		}
+
+   		console.log('Role: ', role);
+   		console.log('Competence_id: ', competence_id);
+   		console.log('Professor_id: ', professor_id);
+   		/*
+   		$.ajax({
+			url: "/entrevista/:user_id/:ev_id/algo-rol",
+			method: "PUT",
+			data: {
+				role: role,
+				competence_id: competence_id,
+				professor_id: professor_id
+			},
+		    success: function(data) {
+		      console.log("Buen cambio rol");
+		    }
+		});
+		*/
+   	});
+
+	$('.inter-level-select').on('change', function() {
+   		level_id = this.value;
+   		competence_id = $(this).data("comp");
+   		professor_id = $(this).data("evaluator");
+
+   		console.log('Level: ', level_id);
+   		console.log('Competence_id: ', competence_id);
+   		console.log('Professor_id: ', professor_id);
+   		/*
+   		$.ajax({
+			url: "/entrevista/:user_id/:ev_id/algo-nivel",
+			method: "PUT",
+			data: {
+				level_id: level_id,
+				competence_id: competence_id,
+				professor_id: professor_id
+			},
+		    success: function(data) {
+		      console.log("Buen cambio nivel");
+		    }
+		});
+		*/
+	});
+
+	$('.inter-retro').bind('input propertychange', function() {
+    	retro = this.value;
+    	competence_id = $(this).data("comp");
+
+    	console.log('Retro: ', retro);
+   		console.log('Competence_id: ', competence_id);
+
+    	/*
+    	$.ajax({
+		url: "/entrevista/:user_id/:ev_id/algo-retro",
+		method: "PUT",
+		data: {
+			competence_id: competence_id,
+			retro: retro
+		},
+	    success: function(data) {
+	      console.log("Buen cambio retro");
+		    }
+		});
+		*/
+
+	    if(this.value.length){
+	    	//$("#yourBtnID").show();
+	    }
+	});
+
+	$('.btn-save-inter').on('click', function() {
+		competence_id = $(this).data("comp");
+		responsible_id = $('.inter-role-select[data-comp="' + competence_id +'"][data-responsible="true"]').data("evaluator");
+		final_level = $('.inter-level-select[data-comp="' + competence_id +'"][data-evaluator="' + 3 + '"]').val();
+
+   		console.log('Final level: ', final_level);
+   		console.log('Competence_id: ', competence_id);
+   		console.log('Responsible: ', responsible_id);
+
+		/*
+		$.ajax({
+		url: "/entrevista/:user_id/:ev_id/algo-evaluacion",
+		method: "PUT",
+		data: {
+			competence_id: competence_id,
+			final_level: final_level
+		},
+	    success: function(data) {
+	      console.log("Buen cambio retro");
+		    }
+		});
+		*/
+	});
+
+});
