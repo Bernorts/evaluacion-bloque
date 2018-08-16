1250a1d3555e1224da1e93e8c14220ab5aa5c8aa
Bernorts <a01209023@itesm.mx>
2018-08-15 19:40:16 -0500

    Remover migracion duplicada

--- db/migrate/20180612223252_add_missing_id_evaluations_users.rb
+++
@@ -1,6 +0,0 @@
-class AddMissingIdEvaluationsUsers < ActiveRecord::Migration[5.0]
-  def change
-  #execute "ALTER TABLE evaluations_users DROP CONSTRAINT evaluations_users_pkey"
-    add_column :evaluations_users, :id, :primary_key
-  end
-end
