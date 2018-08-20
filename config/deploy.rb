dc2310c58fa43469267f8603bd6a28d53113cac9
Bernorts <a01209023@itesm.mx>
2018-08-20 00:26:58 -0500

    Seeds en capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,6 +58,13 @@ namespace :deploy do
     end
   end
 
+  namespace :deploy do
+    desc "reload the database with seed data"
+    task :seed do
+      run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    end
+  end
+
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
