7c29aa2ac9eeb6f3fbae3f352936f8e737371ce0
Bernorts <a01209023@itesm.mx>
2018-11-22 08:21:09 -0600

    Quitar dependent destroy de evaluación a user

--- app/models/evidence.rb
+++ app/models/evidence.rb
@@ -1,6 +1,6 @@
 class Evidence < ApplicationRecord
 
-  belongs_to :user, :dependent => :destroy
+  belongs_to :user
   validates :name, presence: true, length: {minimum: 5, message: 'demasiado corto'}
   validates :url, presence: true, length: { minimum: 10, message: 'demasiado corto'}, format: {with: URI::regexp( %w(http https)), message: 'no es un link'}
   validates :user_id, presence: true
